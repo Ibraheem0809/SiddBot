3d32eb57099264f1e714eff7069435ab902588f5
Md Ibraheem Siddiqui <ibraheemsiddiqui0809@gmail.com>
2026-09-05 01:16:25 +0530

    Fixing Render frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://siddbot.onrender.com/"
+        "https://siddbot.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
